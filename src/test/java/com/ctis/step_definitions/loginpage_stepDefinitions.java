cee2bb3258a747c9c26ce5589fd59a2cdfa6d1cd
Emine Caliskan <caliskanemine95@gmail.com>
2023-03-21 15:21:35 +0300

    Loginpage stepdefinitions class is created and steps are added for login functionality.

---
+++ src/test/java/com/ctis/step_definitions/loginpage_stepDefinitions.java
@@ -0,0 +1,33 @@
+package com.ctis.step_definitions;
+
+import com.ctis.pages.LoginPageElements;
+import com.ctis.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class loginpage_stepDefinitions {
+
+    LoginPageElements loginPageElements= new LoginPageElements();
+
+
+    @When("user enters username {string}")
+    public void user_enters_username(String username) {
+           loginPageElements.inputUsername.sendKeys(username);
+    }
+    @When("user enters password {string}")
+    public void user_enters_password(String password) {
+             loginPageElements.inputPassword.sendKeys(password);
+    }
+    @Then("clicks to the Log In button")
+    public void clicks_to_the_log_in_button() {
+          loginPageElements.loginButton.click();
+    }
+    @Then("user lands on the home page.")
+    public void user_lands_on_the_home_page() {
+     String actualTitle= Driver.getDriver().getTitle();
+     String expectedTitle= "Portal";
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+}
