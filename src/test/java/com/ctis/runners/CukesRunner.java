b703ef4e30b8ef5c5b350917a098c944ee99430e
Şüheda Pehlivan <112968422+suhedapehlivan@users.noreply.github.com>
2023-03-21 23:11:46 +0300

    Update CukesRunner.java

--- src/test/java/com/ctis/runners/CukesRunner.java
+++ src/test/java/com/ctis/runners/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {//"prettiy",
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
