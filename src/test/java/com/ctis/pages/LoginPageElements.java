535963c0d5773e0604e1f64a13381f85f4cc6578
Emine Caliskan <caliskanemine95@gmail.com>
2023-03-21 15:26:23 +0300

    LoginpageElements class is created and web elements are added for login.

---
+++ src/test/java/com/ctis/pages/LoginPageElements.java
@@ -0,0 +1,31 @@
+package com.ctis.pages;
+
+import com.ctis.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPageElements {
+
+    public LoginPageElements(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement inputUsername;
+
+    @FindBy(name = "USER_PASSWORD")
+    public WebElement inputPassword;
+
+    @FindBy(className = "login-btn")
+    public WebElement loginButton;
+
+
+
+
+
+
+
+}
