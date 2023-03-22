ff5d07607d7fe2a97175c9b2b6413e9ea1c19627
Halit Oturk <halithamza0920@gmail.com>
2023-03-22 11:56:43 +0300

    created event step

---
+++ src/test/java/com/ctis/step_definitions/event_step.java
@@ -0,0 +1,114 @@
+package com.ctis.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class event_step {
+
+    @When("user click to event button")
+    public void user_click_to_event_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user add event start date")
+    public void user_add_event_start_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user add event start time")
+    public void user_add_event_start_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user add event end date")
+    public void user_add_event_end_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user add event end time")
+    public void user_add_event_end_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user specify the time zone")
+    public void user_specify_the_time_zone() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user click all day radiobutton")
+    public void user_click_all_day_radiobutton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("user click set reminder button")
+    public void user_click_set_reminder_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user set remind count")
+    public void user_set_remind_count() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user set remind type")
+    public void user_set_remind_type() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user set event location")
+    public void user_set_event_location() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Then("user click members bar")
+    public void user_click_members_bar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user add {string}")
+    public void user_add(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user click Employees and department button")
+    public void user_click_employees_and_department_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user click {string} radiobutton")
+    public void user_click_radiobutton(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Then("click send button")
+    public void click_send_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click event name bar")
+    public void user_click_event_name_bar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user write event name")
+    public void user_write_event_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user cancel event")
+    public void user_cancel_event() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+}
