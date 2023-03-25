6193c1b665c1ccf542baded69df3c2bc1bbc298f
kazim barsbay <barsbaykazim@hotmail.com>
2023-03-25 14:23:51 +0300

    smth updated

--- src/test/java/com/ctis/step_definitions/AddingSmthAppreciation.java
+++ src/test/java/com/ctis/step_definitions/AddingSmthAppreciation.java
@@ -1,4 +1,42 @@
 package com.ctis.step_definitions;
 
+import com.ctis.pages.LoginPageElements;
+import com.ctis.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class AddingSmthAppreciation {
+    @When("User clicks on More button")
+    public void user_clicks_on_more_button() {
+
+    }
+    @When("User clicks on Appreciation button")
+    public void user_clicks_on_appreciation_button() {
+
+    }
+    @When("User writes message")
+    public void user_writes_message() {
+
+    }
+    @When("User clicks on mention")
+    public void user_clicks_on_mention() {
+
+    }
+    @Then("User chooses hr1@cybertekschool.com from list")
+    public void user_chooses_hr1_cybertekschool_com_from_list() {
+
+
+    }
+    @Then("User can see the hr1@cybertekschool.com as mention beside message")
+    public void user_can_see_the_hr1_cybertekschool_com_as_mention_beside_message() {
+
+    }
+
+
+
+
+
+
 }
--- src/test/resources/features/addingSmthAppreciation.feature
+++ src/test/resources/features/addingSmthAppreciation.feature
@@ -1,6 +1,6 @@
+
 Feature: As a user,I should be able to add link, insert video, mention,
   quote, add tag in Appreciation
-
 
   Background: user is already logged in
     Given user is on the login page
@@ -8,4 +8,29 @@ Feature: As a user,I should be able to add link, insert video, mention,
     And user enters password "UserUser"
     Then clicks to the Log In button
     And user lands on the home page.
+
+Scenario: User should be able to add mentions in appreciation
+  When User clicks on More button
+  And User clicks on Appreciation button
+  And User writes message
+  And User clicks on mention
+  Then User chooses hr1@cybertekschool.com from list
+  And User can see the hr1@cybertekschool.com as mention beside message
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
