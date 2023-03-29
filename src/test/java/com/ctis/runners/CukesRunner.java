8b4bac704bc9f46bc426dbbfa052bffa5e70e259
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2023-03-29 23:03:27 +0300

    All features, step definitions created, web elements located inside page class

--- src/test/java/com/ctis/runners/CukesRunner.java
+++ src/test/java/com/ctis/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ctis/step_definitions",
         dryRun = false,
-        tags = "@now",
+        tags = "",
         publish = true
 )
 public class CukesRunner {
